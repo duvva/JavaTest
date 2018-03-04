bd4beb6967e7eea87800f3e87ee8f08a9724b993
duvva <kamhlakarr@gmail.com>
2018-03-04 16:54:52 -0500

    First Commit

---
+++ src/main/java/com/springboot/samples/bootsamples/BootsamplesApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.samples.bootsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class BootsamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsamplesApplication.class, args);
+		
+	}
+
+}

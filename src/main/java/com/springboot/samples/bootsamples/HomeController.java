82af0a46cd2b627d140756bba8e573a65589ac4e
duvva <kamhlakarr@gmail.com>
2018-03-04 16:52:36 -0500

    First Commit

---
+++ src/main/java/com/springboot/samples/bootsamples/HomeController.java
@@ -0,0 +1,11 @@
+package com.springboot.samples.bootsamples;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class HomeController {  
+    @RequestMapping("/hello")  
+    public String hello(){  
+        return"Hello!";  
+    }  
+} 
